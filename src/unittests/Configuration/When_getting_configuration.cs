185dcb647fcdd1e8e7e1122db576c681e640950f
Yves Goeleven <yves@goeleven.com>
2017-01-20 23:22:28 +0100

    some cleanup

--- src/unittests/Configuration/When_getting_configuration.cs
+++ src/unittests/Configuration/When_getting_configuration.cs
@@ -1,17 +1,12 @@
-using System;
 using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 using MessageHandler.EventProcessing.Runtime.Configuration;
-using MessageHandler.EventProcessing.Runtime.Serialization;
 using Xunit;
 
-namespace unittests.Serialization
+namespace unittests.Configuration
 {
     public class When_getting_configuration
     {
-        const string json = "[{\"SomeProperty\":\"test1\"},{\"SomeProperty\":\"test2\"}]";
-
         [Fact]
         public async Task Can_get_object_from_file()
         {
