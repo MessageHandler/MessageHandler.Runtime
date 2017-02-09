84b9c6ef0839d4376b553bc260e61d3d6162a4cd
Joachim Goris <gorisjoachim@hotmail.com>
2017-02-09 09:17:46 +0100

    split container and settings by responsibilities

--- src/runtime/ConfigurationSettings/ISettings.cs
+++ src/runtime/ConfigurationSettings/ISettings.cs
@@ -2,20 +2,27 @@ using System;
 
 namespace MessageHandler.EventProcessing.Runtime.ConfigurationSettings
 {
-    public interface ISettings
+    public interface ISettings:IManageSettings,IProvideSettings
+    {    }
+
+    public interface IProvideSettings
     {
-        void Set(string key, object value);
-        void Set<T>(T value);
         T Get<T>();
         T Get<T>(string key);
         object Get(Type type);
         object Get(string key);
-        void SetDefault<T>(T value);
-        void SetDefault(string key, object value);
         object GetDefault(Type type);
         T GetDefault<T>(string key);
         object GetDefault(string key);
         T GetDefault<T>();
+    }
+
+    public interface IManageSettings
+    {
+        void Set(string key, object value);
+        void Set<T>(T value);
+        void SetDefault<T>(T value);
+        void SetDefault(string key, object value);
         void Remove<T>();
         void Remove(Type type);
         void Remove(string key);
--- src/runtime/Container/IContainer.cs
+++ src/runtime/Container/IContainer.cs
@@ -3,7 +3,19 @@ using System.Collections.Generic;
 
 namespace MessageHandler.EventProcessing.Runtime
 {
-    public interface IContainer
+    public interface IContainer:IResolveDependencies,IRegisterDependencies
+    {}
+
+    public interface IResolveDependencies
+    {
+        T Resolve<T>();
+        object Resolve(Type type);
+
+        IEnumerable<T> ResolveAll<T>();
+        IEnumerable<object> ResolveAll(Type type);
+    }
+
+    public interface IRegisterDependencies
     {
         void Register<T>();
         void Register<T>(Func<T> factory);
@@ -13,11 +25,5 @@ namespace MessageHandler.EventProcessing.Runtime
         void Register(Type type, Lifecycle lifecycle);
         void Register(Type type, Func<object> factory);
         void Register(Type type, Lifecycle lifecycle, Func<object> factory);
-
-        T Resolve<T>();
-        object Resolve(Type type);
-
-        IEnumerable<T> ResolveAll<T>();
-        IEnumerable<object> ResolveAll(Type type);
     }
 }
