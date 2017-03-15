021740f9c2c0f1999a9e37eb6d61198eb58e72cc
Yves Goeleven <yves@goeleven.com>
2017-03-15 16:57:25 +0100

    routing via gateway must also be supported

---
+++ src/runtime/Configuration/GatewayPartition.cs
@@ -0,0 +1,8 @@
+namespace MessageHandler.Runtime
+{
+    public class GatewayPartition
+    {
+        public string EventhubName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
--- src/runtime/Configuration/HandlerRouting.cs
+++ src/runtime/Configuration/HandlerRouting.cs
@@ -6,5 +6,6 @@ namespace MessageHandler.Runtime
     {
         public IList<InputSubjectFilter> InputSubjectFilters { get; set; } = new List<InputSubjectFilter>();
         public IDictionary<string, IList<OutputSubjectRoute>> OutputSubjectRoutes { get; set; } = new Dictionary<string, IList<OutputSubjectRoute>>();
+        public IList<GatewayPartition> GatewayPartitions { get; set; } = new List<GatewayPartition>();
     }
 }
