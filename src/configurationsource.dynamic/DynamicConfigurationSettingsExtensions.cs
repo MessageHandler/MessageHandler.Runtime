6c4dff88e0e6f7b11ad03898abae27618994a126
Yves Goeleven <yves@goeleven.com>
2017-11-22 00:18:37 +0100

    dynamic configuration

--- src/configurationsource.dynamic/DynamicConfigurationSettingsExtensions.cs
+++ src/configurationsource.dynamic/DynamicConfigurationSettingsExtensions.cs
@@ -4,12 +4,12 @@ namespace MessageHandler.Runtime
 {
     public static class DynamicConfigurationSettingsExtensions { 
 
-        public static void EnableDynamicConfiguration(this ConfigurationRoot configuration)
+        public static void EnableDynamicConfiguration(this ConfigurationRoot configuration, IScriptEngine scriptEngine)
         {
             var container = configuration.GetContainer();
             container.Register<DynamicConfigurationSource>();
             container.Register(() => new InMemoryVariableSource(configuration.Settings.GetUserVariables()));
-            container.Register<RoslynScriptingEngine>();
+            container.Register(() => scriptEngine);
             container.Register<RegexTemplatingEngine>();
         }
     }
