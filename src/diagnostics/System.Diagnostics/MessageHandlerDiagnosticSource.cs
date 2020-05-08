b1855bacceef6dd00077b7a870b329f2d5e3e87c
Yves Goeleven <yves@goeleven.com>
2020-05-08 12:07:24 +0200

    diagnostics source

---
+++ src/diagnostics/System.Diagnostics/MessageHandlerDiagnosticSource.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MessageHandler.Runtime
+{    public class MessageHandlerDiagnosticSource
+    {
+        public const string DiagnosticListenerName = "MessageHandler.Runtime";
+        public const string BaseActivityName = DiagnosticListenerName + ".";
+
+        private static readonly DiagnosticListener _diagnosticListener = new DiagnosticListener(DiagnosticListenerName);
+
+        public Activity Start(string operationName, Activity parent, Func<object> getPayload)
+        {
+            string activityName = BaseActivityName + operationName;
+            var activity = new Activity(activityName);
+
+            activity.SetParentId(parent.TraceId, parent.ParentSpanId, parent.ActivityTraceFlags);
+            if (_diagnosticListener.IsEnabled(activityName))
+            {
+                _diagnosticListener.StartActivity(activity, getPayload());
+            }
+            else
+            {
+                activity.Start();
+            }
+
+            return activity;
+        }
+
+        public void Stop(Activity activity, Func<object> getPayload)
+        {
+            if (activity != null)
+            {
+                _diagnosticListener.StopActivity(activity, getPayload());
+            }
+        }
+    }
+}
